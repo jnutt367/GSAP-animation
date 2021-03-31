8840baf950f44d0aaff651fb88bc30d6b5bd5cb8
Jason Nutt <jnutt367@gmail.com>
2021-03-31 17:20:25 -0400

    commit that would help message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 gsap.from("#logo", { duration: 3, x: 300, opacity: 0, scale: 0.5 });
 
 gsap.to("h2.title", { duration: 1, opacity: 0.3 });
-gsap.to(".box", { duration: 2, x: 300 });
+gsap.to(".box", { duration: 5, x: 300 });
 gsap.to(".green", { duration: 3, rotation: 360, scale: 0.5 });
