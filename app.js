c8f0c29e9b1486e6cf7bfd22f94f7b3433c056f7
Jason Nutt <jnutt367@gmail.com>
2021-04-01 14:51:44 -0400

    commit to add different speeds for animations

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 gsap.from("#logo", { duration: 3, x: 300, opacity: 0, scale: 0.5 });
 
-gsap.to("h2.title", { duration: 1, opacity: 0.3 });
-gsap.to(".box", { duration: 5, x: 140 });
-gsap.to(".green", { duration: 3, rotation: 360, scale: 0.5 });
-gsap.to(".orange", { duration: 3, rotation: 360, scale: 0.5 });
-gsap.to(".black", { duration: 3, rotation: 360, scale: 0.5 });
+gsap.to("h2.title", { duration: 5, opacity: 0.3 });
+gsap.to(".box", { duration: 3, x: 140 });
+gsap.to(".green", { duration: 1, rotation: 360, scale: 0.5 });
+gsap.to(".orange", { duration: 7, rotation: 360, scale: 0.9 });
+gsap.to(".black", { duration: 10, rotation: 360, scale: 0.5 });
 
-gsap.to("#world", { duration: 2, x: 150, ease: "bounce" });
+gsap.to("#world", { duration: 2, x: 50, ease: "bounce" });
