541a4c88e2d4e955d42b1e2262070b71c7515985
Jason Nutt <jnutt367@gmail.com>
2021-03-31 21:23:06 -0400

    commit new animations

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ gsap.from("#logo", { duration: 3, x: 300, opacity: 0, scale: 0.5 });
 gsap.to("h2.title", { duration: 1, opacity: 0.3 });
 gsap.to(".box", { duration: 5, x: 300 });
 gsap.to(".green", { duration: 3, rotation: 360, scale: 0.5 });
+gsap.to(".orange", { duration: 3, rotation: 360, scale: 0.5 });
+gsap.to(".black", { duration: 3, rotation: 360, scale: 0.5 });
